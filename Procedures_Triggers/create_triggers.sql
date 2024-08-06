01f2169596eea368479eb06d259771583c0278f2
SalonenAntti <salonen.j.antti@gmail.com>
2024-08-06 20:01:35 +0300

    Update create_triggers.sql

--- Procedures_Triggers/create_triggers.sql
+++ Procedures_Triggers/create_triggers.sql
@@ -1,1 +1,25 @@
+-- =====================================
+--  Trigger Function
+-- =====================================
 
+-- Trigger function for discounts
+ALTER TABLE orders
+ADD COLUMN discount DECIMAL(5, 2);
+
+CREATE OR REPLACE FUNCTION applydiscount()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.totalamount > 1000 THEN
+        NEW.discount = 0.10; -- 10% discount
+    ELSE
+        NEW.discount = 0;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER discounttrigger
+BEFORE INSERT OR UPDATE ON orders
+FOR EACH ROW
+EXECUTE FUNCTION applydiscount();
