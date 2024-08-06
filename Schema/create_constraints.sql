7059e8f3fd18a8176e91295b99d5b6890a5f1371
SalonenAntti <salonen.j.antti@gmail.com>
2024-08-06 17:33:11 +0300

    Update create_constraints.sql

--- Schema/create_constraints.sql
+++ Schema/create_constraints.sql
@@ -1,1 +1,37 @@
+-- =====================================
+--  Constraints
+-- =====================================
 
+-- Suppliers table
+SELECT * FROM suppliers
+ALTER TABLE suppliers
+ADD CONSTRAINT unique_contactemail 
+UNIQUE (contactemail);
+
+ALTER TABLE suppliers
+ADD CONSTRAINT unique_phone
+UNIQUE (phone);
+
+-- Orderitems table
+SELECT * FROM orderitems
+ALTER TABLE orderitems
+ADD CONSTRAINT quantitynotzero
+CHECK (quantity > 0);
+
+ALTER TABLE orderitems
+ADD CONSTRAINT unitpricenotzero
+CHECK (unitprice > 0);
+
+-- Payments table
+SELECT * FROM payments
+ALTER TABLE payments
+ALTER COLUMN Transactionid SET NOT NULL;
+
+-- Shipping table
+SELECT * FROM shipping
+ALTER TABLE shipping
+ALTER COLUMN trackingnumber SET NOT NULL;
+
+ALTER TABLE shipping
+ADD CONSTRAINT shippingcostnotnegative
+CHECK (shippingcost >= 0);
